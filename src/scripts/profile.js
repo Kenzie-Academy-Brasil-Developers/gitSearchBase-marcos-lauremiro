3aa53942f16a92f6eb419ac850f5482ffef0e333
lauremiro <lauremiromelo@gmail.com>
2023-04-26 16:27:15 -0300

    corrigindo aminhos

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -31,7 +31,7 @@ export const renderProfile = (objProfile) => {
     buttonHeader.innerText = 'Trocar de usuario'
 
     buttonHeader.addEventListener('click', () => {
-        window.location.assign('http://127.0.0.1:5500/index.html')
+        window.location.replace('http://127.0.0.1:5500/index.html')
     })
 }
 
